22ab420f9d6549131e9d77b02b603af17023492d
frank6372 <francis-masse@hotmail.com>
2022-11-29 19:55:48 -0400

    Navbar file created
    Nav Bar file created and added to home.

---
+++ web-app-project/pages/Navbar.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css'
+
+
+const Navbar = () => {
+        return(
+            <div>
+                <ul className="navul">
+                    <li id='change' className="navli">About</li>
+                    <li className="navli">Mission</li>
+                    <li className="navli">Contact</li>
+                </ul>
+            </div>
+        )
+    }
+
+
+
+
+
+export default Navbar;
---
+++ web-app-project/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from './Navbar'
+
+
+export default function Home() {
+  return (
+   <div>
+    < Navbar/>
+    <h1>Frank Masse</h1>
+   </div>
+  )
+}
